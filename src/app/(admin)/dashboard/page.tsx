831c5ca96232b7c8b0ebb22e81ae3cc93177ae47
Owl <devowl094@gmail.com>
2026-07-21 20:03:05 +0900

    chore: init

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { LogoHorizontal } from "@/ui/components/Logo/LogoHorizontal";
+
+export default function Page() {
+  return (
+    <div className="w-[400px] overflow-hidden">
+      <LogoHorizontal />
+    </div>
+  );
+}
