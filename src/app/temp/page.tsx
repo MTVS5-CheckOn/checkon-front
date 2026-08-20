5f72a637202ff4cc52cda810105ab4bea6a7cae3
la094 <devowl094@gmail.com>
2026-08-21 00:45:07 +0900

    feat(학생상세: 과제 결과 - 학생 정보 카드

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -6,7 +6,10 @@ export default function Page() {
   return (
     <div
       className={cn(
-        "flex h-screen w-full flex-col items-center gap-4 overflow-y-auto p-8",
+        // 1. Layout
+        "flex h-screen w-full flex-col items-center gap-4 p-8",
+        // 6. Utility
+        "overflow-y-auto",
       )}
     >
       asd
